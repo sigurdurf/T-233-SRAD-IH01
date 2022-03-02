293cede7efed27f02d95d803b8d41c1899d862e2
Sigurður Baldvin Friðriksson <sbf@secureit.is>
2022-03-02 00:50:51 +0000

    Added fizzbuzz all test pass

--- sample.py
+++ sample.py
@@ -2,5 +2,12 @@
 def fizz_buzz(r):
     result = []
     for x in range(1, r+1):
-        result.append(str(x))
+        fizz_str = ""
+        if x % 3 == 0:
+            fizz_str += "fizz"
+        if x % 5 == 0:
+            fizz_str += "buzz"
+        if len(fizz_str) == 0:
+            fizz_str += str(x)
+        result.append(fizz_str)
     return " ".join(result)
