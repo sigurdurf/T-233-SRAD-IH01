07538702f5aff59e022516f7c8790a4244b5d18a
Sigurður Baldvin Friðriksson <sbf@secureit.is>
2022-03-02 00:50:24 +0000

    Fixed test formatting

--- test_sample.py
+++ test_sample.py
@@ -11,8 +11,7 @@ def test_one_to_fifteen():
 
 def test_one_to_hundred():
     fizz_buzz_string = fizz_buzz(100)
-    assert fizz_buzz_string == "1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizz buzz 16 17 fizz 19 buzz fizz 22 23 fizz buzz 26 fizz 28 29 fizz buzz 31 32 fizz 34 buzz fizz 37 38 fizz buzz 41 fizz 43 44 fizz buzz 46 47 fizz 49 buzz fizz 52 53 fizz buzz 56 fizz 58 59 fizz buzz 61 62 fizz 64 buzz fizz 67 68 fizz buzz 71 fizz 73 74 fizz buzz 76 77 fizz 79 buzz fizz 82 83 fizz buzz 86 fizz 88 89 fizz buzz 91 92 fizz 94 buzz fizz 97 98 fizz buzz"
-
+    assert fizz_buzz_string == "1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz fizz 22 23 fizz buzz 26 fizz 28 29 fizzbuzz 31 32 fizz 34 buzz fizz 37 38 fizz buzz 41 fizz 43 44 fizzbuzz 46 47 fizz 49 buzz fizz 52 53 fizz buzz 56 fizz 58 59 fizzbuzz 61 62 fizz 64 buzz fizz 67 68 fizz buzz 71 fizz 73 74 fizzbuzz 76 77 fizz 79 buzz fizz 82 83 fizz buzz 86 fizz 88 89 fizzbuzz 91 92 fizz 94 buzz fizz 97 98 fizz buzz"
 def test_range():
     fizz_buzz_string = fizz_buzz(10)
     fizz_buzz_length = len(fizz_buzz_string.split(" "))
