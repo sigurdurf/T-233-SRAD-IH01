baf1f6ca782c4707754400cc43cb99f7fd52d120
Sigurður Baldvin Friðriksson <sbf@secureit.is>
2022-03-02 00:34:42 +0000

    Added test and verified range for the fizzbuzz game is correct

---
+++ sample.py
@@ -0,0 +1,6 @@
+
+def fizz_buzz(r):
+    result = []
+    for x in range(1, r+1):
+        result.append(str(x))
+    return " ".join(result)
--- test_sample.py
+++ test_sample.py
@@ -4,3 +4,8 @@ from sample import fizz_buzz
 def test_one_to_ten():
     fizz_buzz_string = fizz_buzz(10)
     assert fizz_buzz_string == "1 2 fizz 4 buzz fizz 7 8 fizz buzz"
+
+def test_range():
+    fizz_buzz_string = fizz_buzz(10)
+    fizz_buzz_length = len(fizz_buzz_string.split(" "))
+    assert fizz_buzz_length == 10
