a09ee881ed695ff7d64d19b663d5fae9eaa89537
Sigurður Baldvin Friðriksson <sbf@secureit.is>
2022-03-03 14:23:04 +0000

    cleanup

--- test_sample.py
+++ test_sample.py
@@ -29,4 +29,4 @@ def test_range_some_string():
 def test_range_with_float():
     fizz_buzz_string = fizz_buzz(37.39283238638603)
     assert fizz_buzz_string == "1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz fizz 22 23 fizz buzz 26 fizz 28 29 fizzbuzz 31 32 fizz 34 buzz fizz 37"
-    
+ 
